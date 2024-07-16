a1cb6b6d4e8ffb116ff72eb9a4c127941ea6ffdd
Simply92 <pedrouner92@gmail.com>
2024-07-16 19:21:15 -0300

    cambios 2

--- client/src/views/auth/NewPassword.jsx
+++ client/src/views/auth/NewPassword.jsx
@@ -3,7 +3,7 @@ export default function NewPassword() {
 
     return (
         <>
-            <h1 className="text-5xl font-black">Reestablecer Password</h1>
+            <h1 className="text-4xl font-black">Reestablecer Password</h1>
 
         </>
     )
