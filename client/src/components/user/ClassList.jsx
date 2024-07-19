336827cb3b33c3e50083e81882ef7f01de761022
Marianotd <marianotorresdistefano@gmail.com>
2024-07-19 19:47:19 -0300

    Avance en diseño de calendario

--- client/src/components/user/ClassList.jsx
+++ client/src/components/user/ClassList.jsx
@@ -3,7 +3,7 @@ import ClassCard from './ClassCard'
 
 export default function ClassList({ nextClasses }) {
     return (
-        <div className='w-2/4 flex flex-col gap-7'>
+        <div className='w-6/12 flex flex-col gap-7'>
             {
                 nextClasses.map(clase => {
                     return (
--- client/src/components/user/UserCalendar.jsx
+++ client/src/components/user/UserCalendar.jsx
@@ -1,22 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './customCalendar.css';
 
 export default function UserCalendar() {
-    const [startDate, setStartDate] = useState(new Date())
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDayClick = (value) => {
+        setSelectedDate(value);
+    };
+
+    const checkCondition = (date) => {
+        return true;
+    };
+
+    const handleDayClass = ({ date }) => {
+        let dayClasses = ' ';
+
+        if (date.toDateString() === selectedDate.toDateString()) {
+            dayClasses += ' selected-day';
+        }
+        if (checkCondition(date)) {
+            dayClasses += 'condition-day';
+        }
+        if (date.getDay() === 0 || date.getMonth() !== selectedDate.getMonth()) {
+            dayClasses += ' !text-zinc-300';
+        }
+        if (date.toDateString() === new Date().toDateString()) {
+            dayClasses += ' today';
+        }
+        return dayClasses;
+    }
 
     return (
-        <div>
+        <div className="w-full flex justify-center">
             <Calendar
-                activeStartDate={startDate}
-                onChange={setStartDate}
-                value={startDate}
                 calendarType='gregory'
-                className='border-2 border-yellow-300 rounded-xl p-6'
-                showNavigation={false}
-                onClickDay={(value, event) => console.log('Clicked day: ', value)}
-                tileClassName={''}
+                className={'customCalendar !border-0 !w-full'}
+                // onClickDay={handleDayClick}
+                tileClassName={handleDayClass}
+                formatShortWeekday={(locale, date) => ['DO', 'LU', 'MA', 'MI', 'JU', 'VI', 'SA'][date.getDay()]}
             />
         </div>
-
-    )
+    );
 }
