68d754cc0f8c0a5b216571d8495882a3090638b4
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-07-29 16:28:33 -0400

    Dependenci user en useEffect

--- client/src/views/Resources/Recursos.jsx
+++ client/src/views/Resources/Recursos.jsx
@@ -20,7 +20,7 @@ export default function Recursos() {
       }
       getResources()
     }
-  }, [refreshCards])
+  }, [user, refreshCards])
 
   const handleCreateResource = async () => {
     setModalStatus(true)
