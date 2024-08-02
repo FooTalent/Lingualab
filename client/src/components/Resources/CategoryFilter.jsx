8c2c2c02f5a21ed725d338ad6d7acb746e1d0eaa
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-08-02 16:00:48 -0400

    Modificacion de filtros

--- client/src/components/Resources/CategoryFilter.jsx
+++ client/src/components/Resources/CategoryFilter.jsx
@@ -1,31 +1,37 @@
-import { useState } from "react"
+import musicIcon from './assets/icon-music.svg'
+import puzzleIcon from './assets/icon-puzzle.svg'
+import controllerIcon from './assets/icon-controller.svg'
+import bookMusicIcon from './assets/icon-book-music.svg'
+import movieIcon from './assets/icon-movie.svg'
+import dictionaryIcon from './assets/icon-dictionary.svg'
 
-export default function CategoryFilter({onClick, resource}) {
+export default function CategoryFilter({onClick, resource, isSelected}) {
 
-  const [isSelected, setIsSelected] = useState(false)
+  const ICONS = {
+    "Canción": musicIcon,
+    "Ejercicio": puzzleIcon,
+    "Juego": controllerIcon,
+    "Libro/Audio Libros": bookMusicIcon,
+    "Serie/Película": movieIcon,
+    "Diccionario": dictionaryIcon,
+  }
 
-  const handleFilter = (cat) => {
-    setIsSelected(prevIsSelected => {
-      const newIsSelected = !prevIsSelected
-      if (newIsSelected) {
-        onClick(cat)
-      } else {
-        onClick('')
-      }
-      return newIsSelected
-    })
-  
-    if(!isSelected) return onClick(cat)
-    if(isSelected) return onClick('')
+  const handleFilter = () => {
+    onClick(resource)
   }
 
   return (
     <button 
-        onClick={() => handleFilter(resource)}
+        onClick={handleFilter}
         className={
-        `flex items-center rounded-lg py-3 px-6 border 
+        `flex items-center rounded-lg py-3 px-6 border gap-4
         ${isSelected ?  'bg-Purple text-white border-transparent' : 'bg-white text-[#444444] border-[#444444]'}`
     }>
+      <img 
+        src={ICONS[resource]} 
+        alt={`ícono de ${resource}`}
+        className={`${isSelected ? 'text-white' : 'fill-current'}`}
+      />
       {resource}
     </button>
   )
