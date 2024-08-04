ce7f81d206fa33837bf265db073b143233cdb4ab
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-08-04 10:39:36 -0400

    Estilos a Modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -4,11 +4,11 @@ const Modal = ({ isOpen, onClose, title, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg shadow-lg p-4 w-1/2">
+    <div className="fixed inset-0 bg-[#444] bg-opacity-20 flex justify-center items-center z-50">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-2/3 max-w-6xl">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-semibold">{title}</h2>
-          <button onClick={onClose} className="text-gray-700 hover:text-gray-900">&times;</button>
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <button onClick={onClose} className="text-Purple text-4xl hover:text-gray-900">&times;</button>
         </div>
         {children}
       </div>
