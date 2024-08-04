e2f05ffddb8ff5c562630035000661e841dd8509
Simply92 <pedrouner92@gmail.com>
2024-08-04 18:21:25 -0300

    ruta student

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AuthLayout       from './layouts/AuthLayout'
-import Login            from './views/auth/Login'
-import AppLayout        from './layouts/AppLayout'
-import Home             from './views/Home'
-import Register         from './views/auth/Register'
-import ForgotPassword   from './views/auth/ForgotPassword'
-import NewPassword      from './views/auth/NewPassword'
-import Profile          from './views/Profile/Profile'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './views/auth/Login'
+import AppLayout from './layouts/AppLayout'
+import Home from './views/Home'
+import Register from './views/auth/Register'
+import ForgotPassword from './views/auth/ForgotPassword'
+import NewPassword from './views/auth/NewPassword'
+import Profile from './views/Profile/Profile'
 import VirtualClassRoom from './views/Classroom/VirtualClassRoom/VirtualClassRoom'
-import VCRDetail        from './views/Classroom/VirtualClassRoom/Program/ProgramDetail'
-import VCRClassDetail   from './views/Classroom/VirtualClassRoom/Class/ClassDetail'
-import WorkSpace        from './views/Classroom/WorkSpace/WorkSpace'
-import ProgramDetail    from './views/Classroom/WorkSpace/Program/ProgramDetail'
-import ClassDetail      from './views/Classroom/WorkSpace/Class/ClassDetail'
-import Recursos         from './views/Resources/Resources'
-import Calendario       from './views/user/Calendario'
-import Landing          from './views/Landing'
-import History          from './views/History'
+import VCRDetail from './views/Classroom/VirtualClassRoom/Program/ProgramDetail'
+import VCRClassDetail from './views/Classroom/VirtualClassRoom/Class/ClassDetail'
+import WorkSpace from './views/Classroom/WorkSpace/WorkSpace'
+import ProgramDetail from './views/Classroom/WorkSpace/Program/ProgramDetail'
+import ClassDetail from './views/Classroom/WorkSpace/Class/ClassDetail'
+import Recursos from './views/Resources/Resources'
+import Calendario from './views/user/Calendario'
+import Landing from './views/Landing'
+import History from './views/History'
 import AboutUs from './views/AboutUs'
+import ViewStudent from './views/Student/ViewStudent'
 
 export default function Router() {
     return (
@@ -32,8 +33,9 @@ export default function Router() {
                     <Route path='/workspace/programas/:eid' element={<ProgramDetail />} />
                     <Route path='/workspace/class/:eid' element={<ClassDetail />} />
                     <Route path='/recursos' element={<Recursos />} />
-                    <Route path='/profile' element={<Profile />}  />
+                    <Route path='/profile' element={<Profile />} />
                     <Route path='/calendario' element={<Calendario />} />
+                    <Route path='/alumnos' element={<ViewStudent />} />
                 </Route>
 
                 <Route element={<AuthLayout />}>
