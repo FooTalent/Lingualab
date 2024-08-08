513e9c91f8e1a2b65ef6889e2e7d6ed01815bd33
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-08-08 20:53:00 -0300

    Correccion Link Logo

--- client/src/components/LogoHeader.jsx
+++ client/src/components/LogoHeader.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 export default function LogoHeader() {
     return (
-        <img src="/LogoHeader.png" alt="Logotipo empresa" />
+        <Link to={"/"}><img src="/LogoHeader.png" alt="Logotipo empresa" /></Link>
     )
 }
