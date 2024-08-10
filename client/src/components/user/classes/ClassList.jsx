b39d75e0e778375d821573fb5d1dfe9602b499f3
Marianotd <marianotorresdistefano@gmail.com>
2024-08-09 22:41:39 -0300

    Modal Calendario

--- client/src/components/user/classes/ClassList.jsx
+++ client/src/components/user/classes/ClassList.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import ClassCard from './ClassCard';
 
-export default function ClassList({ dayClases, toggleOptions, stateOption }) {
-    console.log(dayClases)
+export default function ClassList({ data, toggleOptions, stateOption }) {
     return (
-        dayClases.map(clase => (
+        data.map(clase => (
             <ClassCard
                 key={clase._id}
                 id={clase._id}
                 title={clase.title}
                 level={clase.level}
-                students={clase.students}
-                date={clase.date}
-                resources={clase.class}
+                students={clase.program.students}
                 duration={clase.duration_card}
-                isNow={clase.isNow}
-                linkMeet={clase.link_meet}
+                date={clase.daytime}
+                program={clase.program}
                 toggleOptions={toggleOptions}
                 stateOption={stateOption}
             />
