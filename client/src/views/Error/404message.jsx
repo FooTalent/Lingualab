a6efa9b6d9c97eddffa8f2a4d1932aed69f0a2fe
Vanessa Sanchez <vanessapulido100@gmail.com>
2024-08-15 14:43:00 -0500

    Front listo en cualquier pagina que arroje error 404

--- client/src/views/Error/404message.jsx
+++ client/src/views/Error/404message.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const The404message = () => {
   return (
-    <h1>ERROR 404! </h1>
-  )
+    <div className="flex items-center justify-center h-screen bg-[#FFDC00]">
+      <img src="/ImagesError404/404.png" alt="Error 404" className="max-w-full h-auto" />
+    </div>
+  );
 }
 
-export default The404message
+export default The404message;
