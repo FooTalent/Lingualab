241a9e3bee7693b355b5309c7ab5d370271b60c2
Marianotd <marianotorresdistefano@gmail.com>
2024-08-15 18:10:13 -0300

    Trabajos en calendario, en base a feedback y Jira

--- client/src/views/Calendar/Calendario.jsx
+++ client/src/views/Calendar/Calendario.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useAppStore } from '../../store/useAppStore';
 import { dayjsLocalizer } from 'react-big-calendar';
 import ClassCalendar from '../../components/user/calendar/ClassCalendar';
-import Modal from '../../components/user/calendar/Modal';
+import ModalCalendar from '../../components/user/calendar/ModalCalendar';
 import dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import { Toast } from '../../utils/toast';
-import { getClassesByTeacherAndDate } from '../../services/programs.services'
+import { deleteClass, getClassesByTeacherAndDate } from '../../services/programs.services'
+import popUp from '/ImagesVCR/EliminarAula.png'
+import ButtonModal from '../../components/Form/ButtonModal';
+import Modal from '../../components/Modal';
 
 dayjs.locale('es');
 const localizer = dayjsLocalizer(dayjs);
@@ -16,11 +19,13 @@ export default function Calendario() {
     const [classes, setClasses] = useState([])
     const [date, setDate] = useState(new Date());
     const [open, setOpen] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
+    const [classIdToDelete, setClassIdToDelete] = useState(null);
 
     useEffect(() => {
         fetchTeacherClasses()
-    }, [])    
-        
+    }, [])
+
     const fetchTeacherClasses = async () => {
         // startDate=2024-07-01&endDate=2024-08-01
         const newClasses = await getClassesByTeacherAndDate(user.token, userDetail._id, '')
@@ -53,6 +58,44 @@ export default function Calendario() {
         setOpen(true);
     };
 
+    const handleDelete = (classId) => {
+        setOpen(false);
+        setDeleteModal(true);
+        setClassIdToDelete(classId);
+    }
+
+    const handleCancelDelete = () => {
+        setDeleteModal(false)
+        setOpen(true)
+    }
+
+    const handleConfirmDelete = async () => {
+        if (!classIdToDelete) return;
+
+        try {
+            const response = await deleteClass(user.token, classIdToDelete);
+            if (!response.isError) {
+                Toast.fire({
+                    title: 'Clase eliminada con éxito',
+                    icon: 'success',
+                });
+                setDeleteModal(false);
+                setOpen(true);
+                fetchTeacherClasses();
+            } else {
+                Toast.fire({
+                    title: 'No se pudo eliminar la clase',
+                    icon: 'error',
+                });
+            }
+        } catch (error) {
+            Toast.fire({
+                title: 'Error en la solicitud de eliminación',
+                icon: 'error',
+            });
+        }
+    }
+
     return (
         <>
             <main className='container mx-auto flex flex-col gap-5'>
@@ -66,14 +109,29 @@ export default function Calendario() {
                 />
             </main>
 
-            <Modal
+            <ModalCalendar
                 open={open}
                 setOpen={setOpen}
                 onNavigate={handleNavigate}
                 label={handleDate(date)}
                 data={classes}
                 selectedDay={date}
+                openModalDelete={handleDelete}
+                confirmDelete={handleConfirmDelete}
             />
+
+            <Modal modalSize={'xsmall'} isOpen={deleteModal}>
+                <div className='flex flex-col gap-8'>
+                    <div className="flex justify-center ">
+                        <img src={popUp} alt="Eliminar aula" />
+                    </div>
+                    <div className="grid grid-cols-2 gap-6">
+                        <ButtonModal buttonAction={handleCancelDelete} type={'prev'} label={'Cancelar'} />
+                        <ButtonModal buttonAction={handleConfirmDelete} label={'Eliminar aula'} />
+                    </div>
+                </div>
+
+            </Modal>
         </>
     );
 }
