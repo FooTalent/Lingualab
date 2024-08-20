3e79a85cd9c62c41ea5504ef528fb83eaab8a573
Henryck Guaramato <guaramatohenryck@gmail.com>
2024-08-19 21:47:17 -0400

    Se elimina buscador en el Home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -46,21 +46,6 @@ const Home = () => {
           <h1 className="text-home leading-9 font-semibold">
             ¡Te damos la bienvenida!
           </h1>
-
-          <div className="flex items-center gap-4">
-            <div className="relative">
-              <input
-                type="text"
-                placeholder="¿Qué estás buscando?"
-                className="border border-Grey rounded-lg px-4 py-3 pl-11 w-[566px] h-[48px] bg-inputBg text-card placeholder:text-Grey outline-none focus:border-Purple hover:border-Purple"
-              />
-              <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#444444]" />
-            </div>
-
-            <button className="bg-Purple tracking-wide hover:bg-PurpleHover text-white font-extrabold px-4 py-3 rounded-lg h-[48px] ease-out duration-600">
-              Buscar
-            </button>
-          </div>
         </div>
 
         <div className="flex flex-col lg:flex-row gap-8">
