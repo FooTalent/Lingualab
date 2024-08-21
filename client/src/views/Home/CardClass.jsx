f88bf5259482b30ce3953fe8f72256d93e35583a
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-08-21 00:48:02 -0300

    Se actualiza Card Home

--- client/src/views/Home/CardClass.jsx
+++ client/src/views/Home/CardClass.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import gettingHourClass from '../../utils/gettingHoursClass';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ShareIcon from '@mui/icons-material/Share';
+import editarPrograma from '/editarPrograma.svg'
+import duplicar from '/duplicar.svg'
+import DeleteIcon from '@mui/icons-material/Delete';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import Options from '../../components/user/classes/Options';
 
-const CardClass = ({ thisclass }) => {
+const CardClass = ({ thisclass, buttonFunction, deleteFunction }) => {
+  const [state, setState] = useState(false);
+
+  
+  const handleOptions = (e) => {
+    e.stopPropagation();
+    setState(!state);
+  };
+
+  const handleButton = (e) => {
+    e.stopPropagation();
+    buttonFunction(thisclass._id);
+  }
+
+  const links = [
+    { function: () => {}, label: <><PersonAddIcon /> Invitar estudiante/s:</>, disabled: true },
+    { path: null, state: 'edit', label: <><ShareIcon /> Compartir</>, disabled: true },
+    { path: `/aulavirtual/aula/${thisclass.program._id}`, state: 'edit', label: <><img src={editarPrograma} alt='Editar aula' />Editar aula</> },
+    { path: ``, label: <><img src={duplicar} alt='Duplicar Programa' />Duplicar aula</>, disabled: true },
+    { path: ``, label: <><DeleteIcon />Eliminar aula</>, function: deleteFunction, disabled: true },
+  ];
+
   return (
-    <div className="shadow-home rounded-lg max-w-[357px] h-[165px] p-4 cursor-pointer ">
+    <div className="shadow-home rounded-lg max-w-[357px] h-[161px] p-4 cursor-pointer flex flex-nowrap">
       <div className="flex flex-col justify-between h-full">
         <h2
-          className="p-0 text-lg font-bold truncate"
-          style={{ maxWidth: 'calc(100% - 8rem)' }}
+          className="p-0 text-base font-bold truncate"
         >
           {thisclass.title}
         </h2>
-        <p className='flex gap-4'>
+        <p className='flex text-sm gap-4'>
           <span className='font-semibold'>Estudiante/s:</span>
           <div>
             {thisclass.program.students.map((student) => (
@@ -19,14 +46,30 @@ const CardClass = ({ thisclass }) => {
             ))}
           </div>
         </p>
-        <p className='flex gap-4'>
+        <p className='flex text-sm gap-4'>
           <span className='font-semibold'>Fecha Inicio: </span>
           { thisclass.daytime ? new Date(thisclass.daytime).toLocaleDateString() : '-'}
         </p>
-        <p className='flex gap-4'>
+        <p className='flex text-sm gap-4'>
           <span className='font-semibold'>Hora:</span>
           { thisclass.daytime ? gettingHourClass(thisclass.daytime, thisclass.program.duration_hours) : '-' }
         </p>
+      </div>
+      <div className="flex flex-col justify-between items-end">
+        <div className='relative'>
+          <button
+            onClick={(e) => handleOptions(e)}
+          >
+            <MoreVertIcon className='text-Purple' />
+          </button>
+          <Options id={thisclass._id} state={state} links={links} />
+        </div>
+        <button
+          className="bg-Purple hover:bg-PurpleHover text-white px-4 py-2 rounded-lg font-extrabold ease-linear duration-150"
+          onClick={(e) => buttonFunction(thisclass._id)}
+        >
+          Ver clases
+        </button>
       </div>
     </div>
   );
