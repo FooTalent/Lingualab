1e23dc02fe7e583d1a184f094fa19a03472242b4
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-08-21 22:48:28 -0300

    el token login pasa a estar por 3 dias

--- backend/src/modules/users/logic/createToken.js
+++ backend/src/modules/users/logic/createToken.js
@@ -3,6 +3,6 @@ import jwt from "jsonwebtoken";
 
 const JWT_PRIVATE_KEY = configEnv.jwt_code;
 
-const createToken = (user) => jwt.sign(user, JWT_PRIVATE_KEY, { expiresIn: "1d" });
+const createToken = (user) => jwt.sign(user, JWT_PRIVATE_KEY, { expiresIn: "3d" });
 
 export default createToken;
